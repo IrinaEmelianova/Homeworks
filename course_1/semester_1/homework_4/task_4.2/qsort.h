f20c91354937996222b4fd2a65c07664c1b8b7e1
IrinaEmelianova <ira_emelianova@mail.ru>
2016-12-10 15:38:08 +0400

    added the finished task 4.2

---
+++ course_1/semester_1/homework_4/task_4.2/qsort.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int supportElement(int array[], int left, int right);
+int partition(int array[], int left, int right, int element);
+void qsort(int array[], int left, int right);
